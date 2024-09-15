bdd200b34979864e6348b0fd3d6ad2c59316ed58
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-09-15 09:12:27 +0545

    added date and moved script

--- script.js
+++
@@ -1,7 +0,0 @@
-function goToTop() {
-    // The offsetTop returns the value top position of the element
-    let scrollDiv = document.getElementById("header").offsetTop;
-  
-    // The windows.scrollTo() method moves the page to a destination element, in this case, the scrollDiv which has the offsetTop value of the header element
-    window.scrollTo({ top: scrollDiv, behavior: "smooth" });
-}
